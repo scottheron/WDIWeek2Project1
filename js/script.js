bc3225f69ccefe8a5104a31d12d89a6530271d31
Scott <scott.heron@outlook.com>
2016-02-01 11:35:31 -0800

    The black deck array sends a random card to the display space and then removes that card from the deck. Once the cards run out a message is displayed.

--- js/script.js
+++ js/script.js
@@ -4,8 +4,11 @@ WDI 06 - Project 1, Cards Against Humanity. script.js file.
 
 $(document).ready(function(){
 
-/*Black card array holding the sentence card values*/
-	var blackCardArray = [
+	/*General variables for counters, random number values etc. */
+	var randomNum;
+
+	/*Black card array holding the sentence card values*/
+	var blackCardArrayInit = [
 		"How did I lose my virginity?",
 		"Why can't I sleep at night?",
 		"What's that smell?",
@@ -16,8 +19,22 @@ $(document).ready(function(){
 		"It's a pitty that kids these days are all getting involved with _________.",
 		"Today on Maury: 'Help! My son is'<br>_________"
 	];
-
-	$('#sentence-space').html('<p>"'+blackCardArray[6]+'"</p>');
+	
+	/*blackcardArray is created from the blackCardArrayInit initialization array for pop purposes*/
+	var blackCardArray = blackCardArrayInit;
+	
+	/*Random number generator picking an entry from blackCardArray[] and displaying that entry to the sentence-space div*/
+	$('#black-card-back').on('click', function(){
+		randomNum = Math.floor(Math.random() * blackCardArray.length);
+		$('#sentence-space').html('<p>"'+blackCardArray[randomNum]+'"</p>');
+		if(blackCardArray.length) {
+			blackCardArray.splice(randomNum, 1);
+			console.log(blackCardArray);
+		}
+		else {
+			$('#sentence-space').html('<p>"Aww no more cards :.("</p>');
+		}
+	});
 
 
 /*jQuery call to flip the card*/
