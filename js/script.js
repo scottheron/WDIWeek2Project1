af50682ac0afd8864a261f648c87b1549819c0bd
Scott <scott.heron@outlook.com>
2016-02-01 12:13:09 -0800

    whiteCardArray is partially populated and I can search through the blackCardArray for the blank string and replace it with the entry from the whiteCardArray.

--- js/script.js
+++ js/script.js
@@ -6,9 +6,10 @@ $(document).ready(function(){
 
 	/*General variables for counters, random number values etc. */
 	var randomNum;
+	var replaceString;
 
 	/*Black card array holding the sentence card values*/
-	var blackCardArrayInit = [
+	var blackCardArray = [
 		"How did I lose my virginity?",
 		"Why can't I sleep at night?",
 		"What's that smell?",
@@ -19,22 +20,55 @@ $(document).ready(function(){
 		"It's a pitty that kids these days are all getting involved with _________.",
 		"Today on Maury: 'Help! My son is'<br>_________"
 	];
+
+	/*White card array holding the answer cards*/
+	var whiteCardArray = [
+		"being on fire",
+		"racism",
+		"old-people smell",
+		"a Micropenis",
+		"women in yogurt commercials",
+		"classist undertones",
+		"not giving a shit about the Third World",
+		"inserting a mason jar into my anus",
+		"court-ordered rehab",
+		"a windmill full of corpses",
+		"the gays",
+		"an oversized lollipop",
+		"african children",
+		"an asymmetric boob job",
+		"bingeing and purging",
+		"the hardworking Mexican",
+		"an Oedipus complex",
+		"a tiny horse",
+		"boogers",
+		"penis envy",
+		"Barack Obama",
+		"my humps",
+		"the Tempur-Pedic Swedish Sleep System",
+		"scientology"
+	]
+
 	
-	/*blackcardArray is created from the blackCardArrayInit initialization array for pop purposes*/
-	var blackCardArray = blackCardArrayInit;
-	
-	/*Random number generator picking an entry from blackCardArray[] and displaying that entry to the sentence-space div*/
+	/*Clicking on the black card div generates a random number which is used to pick an entry from blackCardArray[] and displaying that entry to the sentence-space div. The card is then removed from the deck using .splice. once the last card has been played a message is displayed.*/
 	$('#black-card-back').on('click', function(){
-		randomNum = Math.floor(Math.random() * blackCardArray.length);
-		$('#sentence-space').html('<p>"'+blackCardArray[randomNum]+'"</p>');
 		if(blackCardArray.length) {
+			randomNum = Math.floor(Math.random() * blackCardArray.length);
+			$('#sentence-space').html('<p>"'+blackCardArray[randomNum]+'"</p>');
+			console.log(blackCardArray[randomNum]);
 			blackCardArray.splice(randomNum, 1);
-			console.log(blackCardArray);
 		}
 		else {
 			$('#sentence-space').html('<p>"Aww no more cards :.("</p>');
 		}
 	});
+
+	/*Replacing the underscore blank in the blackCardArray with text from the whiteCardArray*/
+	console.log(blackCardArray[3]);
+	console.log(whiteCardArray[0]);
+	blackCardArray[3] = blackCardArray[3].replace('_________', whiteCardArray[0]); 
+	console.log(blackCardArray[3]);
+
 
 
 /*jQuery call to flip the card*/
