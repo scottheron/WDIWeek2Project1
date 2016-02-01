a433582db49a9bf81038aa09c98c5080404c464b
Scott <scott.heron@outlook.com>
2016-01-31 18:22:04 -0800

    the table top is set up

--- js/script.js
+++ js/script.js
@@ -1,3 +1,14 @@
 /*
 WDI 06 - Project 1, Cards Against Humanity. script.js file.
 */
+
+$(document).ready(function(){
+
+/*jQuery call to flip the card*/
+	$("#card").flip({
+		axis: 'x',
+  		trigger: 'hover'
+	});
+
+
+});
