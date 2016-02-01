5dc391c089fb771a733292c0a90c95c3d4f3960d
Scott <scott.heron@outlook.com>
2016-02-01 10:50:40 -0800

    some font re-sizing on the score box and the sentence space. Sentence space being read to from JS.

--- js/script.js
+++ js/script.js
@@ -4,11 +4,28 @@ WDI 06 - Project 1, Cards Against Humanity. script.js file.
 
 $(document).ready(function(){
 
+/*Black card array holding the sentence card values*/
+	var blackCardArray = [
+		"How did I lose my virginity?",
+		"Why can't I sleep at night?",
+		"What's that smell?",
+		"I got 99 problems but _________ ain't one.",
+		"Maybe she's born with it, maybe it's _________.",
+		"What's the next Happy Meal toy?",
+		"Here is the church, Here is the people, Open the doors, and there is _________.",
+		"It's a pitty that kids these days are all getting involved with _________.",
+		"Today on Maury: 'Help! My son is'<br>_________"
+	];
+
+	$('#sentence-space').html('<p>"'+blackCardArray[6]+'"</p>');
+
+
 /*jQuery call to flip the card*/
+/*
 	$("#card").flip({
 		axis: 'x',
   		trigger: 'hover'
 	});
-
+*/
 
 });
